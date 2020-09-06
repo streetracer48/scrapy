44585aa0fdd4582c13ef0ed21a7142ebf51ad3d9
M Karim <38099690+streetracer48@users.noreply.github.com>
2020-09-06 22:13:05 +0300

    Create scrapy.py

---
+++ scrapy.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "https://kodit.io/en/apartments-for-sale"
+
+# Connect to the website and return the html to the variable ‘page’
+try:
+    page = urlopen(url)
+except:
+    print("Error opening the URL")
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(page, 'html.parser')
+
+# Take out the <div> of name and get its value
+content = soup.find('div', {"class": "Wrap"})
+
+article = ''
+for i in content.findAll('p'):
+    article = article + ' ' +  i.text
+print(article)
+
+# Saving the scraped text
+with open('scraped_text.txt', 'w') as file:
+    file.write(article)
